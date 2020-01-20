b2161a6e68d89ab6864a63285fe5814e4b29eddb
RogerAirgood <rogerairgood@gmail.com>
2020-01-19 20:34:22 -0800

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -2,7 +2,8 @@
 sysrc lighttpd_enable=YES
 sysrc php_fpm_enable=YES
 # Enable SSL with a self signed cert. It is mandatory for Chrome to work with PrivateBin
-openssl req -new -newkey rsa:4096 -days 18250 -nodes -x509 -subj "/C=privatebin/ST=privatebin/L=privatebin/O=privatebin/CN=privatebin" -keyout privatebin.key -out privatebin.cert
+openssl genrsa -rand -genkey -out cert.key 2048
+openssl req -new -x509 -days 365 -key cert.key -out cert.crt -sha256 -subj "/C=pbin/ST=pbin/L=pbin/O=pbin/CN=pbin"
 # Wait for SSL to finish its thing
 sleep 10
 cat privatebin.key privatebin.cert > privatebin.pem
